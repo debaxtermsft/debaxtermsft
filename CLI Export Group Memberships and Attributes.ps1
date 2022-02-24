c814bbf900f69c7afdbe70ab86360e1e8daddc83
debaxtermsft <92338056+debaxtermsft@users.noreply.github.com>
2022-02-24 11:49:18 -0500

    Add files via upload
    CLI - Export Group Memberships and Attributes.ps1
    Script to export Group members and Attributes using CLI only
    ex: 
    '.\CLI Export Group Memberships and Attributes.ps1' -mainmenu 'Group Members' -GroupTypeFilter Assigned -SorOGroup Azure -OutputFile "C:\TEMP\CLI GM ASSIGNED AZURE.CSV"
    -mainmain (options "Group Members","Groups Attributes")
    -GroupFilterType (options "All","Assigned","Dynamic")
    -SorOGroup (options "All","Azure","Office")
    -OutputFile enter in quotes "directory\filename.csv"
    
    UI Export Group Memberships and Attributes v8.ps1 
    UI form which gives the user the ability to pick the same as the CLI version along with Selection capability to choose which groups memberships or attributes they want to export.
    UI Export RBAC gives the admin the ability to export all RBAC Assigned Roles to 
    All Tenants (signed into)
    All Subscriptions (the admin is able to view)
    Ask you were to save the file(s).  Files are automatically saved by tenant/subscription/selection/time and date.csv
    
    and will iterate through with the options 
    All RBAC – this will export All RBAC assignments in the currently selected Tenant
    All Users – All User assignments for the tenant are exported
    All Groups – All Group assignments for the tenant are exported
    All SPN/apps – All SPN/App assignments for the tenant are exported
    All Unknowns – All “Identity Unknowns” for the tenant are exported – very useful if a security group is deleted or if a customer deletes/moves a tenant.
    Select an Option and hit OK ( Cancel from Any option will exit the script)
    You are then prompted how you want the data sorted (whichever makes viewing easier) by DisplayName, Scope, or RoleDefinitionName.

---
+++ CLI Export Group Memberships and Attributes.ps1
@@ -0,0 +1,197 @@
+﻿#######################
+# Written by Derrick Baxter debaxter@microsoft.com
+#the below script uses the AZ powershell module pulling all roleassignments for groups
+#
+# 2/24/2022
+#debaxter@microsoft.com
+#complete rewrite
+# Group Members/Group Attributes
+# Group Members and Group Attributes filter1- All/Assigned/Dynamic
+# Group Members - All (gets all group members of all Groups)
+# Group Members - Assigned Filters - All(assigned)/Azure/Office
+# Group Members - Dynamic Filter - All(dynamic)/Azure/Office
+# Group Attributes - All (all groups)/Assigned (All assigned group attributes)/ Dynamic (all Dynamic Group Attributes)
+#Using Powershell AzureADPreview version 2.0.138.0
+#CLI Version
+#######################
+
+
+    param([parameter(mandatory)][validateset("Group Members","Groups Attributes")][string] $mainmenu,
+          [parameter(mandatory)][validateset("All","Assigned","Dynamic")] [string]$GroupTypeFilter,
+          [parameter(mandatory)][validateset("All","Azure","Office")] [string]$SorOGroup,
+          [parameter(mandatory)] [string]$OutputFile)
+
+
+#remove the rem below and enter the tenant id, and rem the 2nd connect-azaccount
+try
+    {
+    Get-AzureADDomain -ErrorAction Stop > $null
+    }
+catch
+    {
+    connect-azuread
+    }
+
+
+   
+ 
+
+
+    
+    #$MainMenuQuestion  
+    #$GroupTypeQuestion
+    #$SorOQuestion  
+
+    
+    $groupoutput =@()
+    $groupquestion =@()
+    $grouptype =@()
+    $group  =@()
+    $groupmembers =@()
+    $groupinfo =@()
+    $groupinfo2 =@()
+
+    if ($MainMenuQuestion -eq "Group Members")
+    {
+    write-host "got here" $MainMenuQuestion
+        if($GroupTypeQuestion-eq "Dynamic") #all dynamic group members
+        {
+        write-host "got here " $GroupTypeQuestion
+            if($SorOQuestion-eq "Azure")
+            {
+            $group = get-azureadmsgroup -all $true | 
+                ?{($_.grouptypes -contains "DynamicMembership" -and $_.grouptypes -notcontains "Unified" -and $_.securityenabled -eq $true) }| 
+                select displayname, id, description | 
+                Sort-Object DisplayName
+            }
+            elseif($SorOQuestion-eq "Office")
+            {and I
+            $group = get-azureadmsgroup -all $true | 
+                ?{($_.grouptypes -contains "DynamicMembership" -and $_.grouptypes -contains "Unified"-and $_.securityenabled -eq $true) }| 
+                select displayname, id, description | 
+                Sort-Object DisplayName
+            }
+
+            else
+            {
+            $group = get-azureadmsgroup -all $true | 
+                select displayname, id, description | 
+                Sort-Object DisplayName
+            }
+        }
+        elseif($GroupTypeQuestion -eq "Assigned") #all assigned group members
+        {
+        write-host "got here assigned"
+            if($SorOQuestion -eq "Azure")
+            {
+            write-host "got here " $GroupTypeQuestion
+                $group = get-azureadmsgroup -all $true | 
+                    ?{($_.GroupTypes.Count -eq 0 -or $_.grouptypes -notcontains "DynamicMembership"  -and $_.grouptypes -notcontains "Unified") }| 
+                    select displayname, id, description | 
+                    Sort-Object DisplayName
+            }
+            elseif($SorOQuestion-eq "Office")
+            {
+            write-host "got here " $GroupTypeQuestion
+                $group = get-azureadmsgroup -all $true | 
+                    ?{($_.GroupTypes.Count -eq 0 -or $_.grouptypes -notcontains "DynamicMembership"  -and $_.grouptypes -contains "Unified") }| 
+                    select displayname, id, description | 
+                    Sort-Object DisplayName
+            }
+
+            else
+            {
+            write-host "got here all"
+                $group = get-azureadmsgroup -all $true | 
+                    ?{($_.GroupTypes.Count -eq 0 -or $_.grouptypes -notcontains "DynamicMembership") }| 
+                    Sort-Object DisplayName
+             }
+
+        }
+        else # must be All group members
+        {
+                $group = get-azureadmsgroup -all $true | 
+                    select displayname, id, description | 
+                    Sort-Object DisplayName
+        }
+    
+        
+            foreach ($item in $group)
+            {
+                $findgroupmembers = get-azureadgroupmember -ObjectId $item.id | select displayname, objectid, userprincipalname, UserType, objecttype | Sort-Object DisplayName
+                if($findgroupmembers -eq 0) 
+                {
+                        $groupinfo2 = $item.DisplayName +","+ $item.Description +"," + $item.ObjectId + ",No Members"
+                        $groupinfo += $groupinfo2
+                }        
+
+                else
+                {
+                foreach ($item4 in $findgroupmembers)
+                    {
+                        #$item3.DisplayName, $item3.id, $item4.DisplayName
+                        $groupinfo2 = $item.DisplayName +","+ $item.Description +"," + $item.Id + ","+ $item4.displayName +","+ $item4.objectid +","+ $item4.userprincipalname  +","+ $item4.UserType +","+ $item4.objecttype
+                        $groupinfo += $groupinfo2
+                    }
+                }
+            }
+         
+        #creating header for CSV
+        $groupoutput = "'Group Display Name','Group Description','Group ObjectID','Member DisplayName','Member ObjectID','Member UserPrincipalName','User Type','Object Type"
+        #$file = $MainMenuQuestion +"_"+ $GroupTypeQuestion+"_"+ $SorOQuestion
+        #$OutputFile = save-file -filename $file -initialDirectory $env:HOMEDRIVE
+        #if ($OutputFile -eq "Cancel"){break}
+        $groupoutput |  Out-File -FilePath $outputfile -Encoding utf8 -Force
+        #adding info to CSV
+        $groupinfo | Out-File -FilePath $outputfile -Encoding utf8 -Append
+    }
+    else #  Group attributes ($MainMenuQuestion -eq "Group Attributes") #used to backup all information about a group needed to recreate assigned group, especially dynamic security groups w rule
+    {
+        if($GroupTypeQuestion-eq "Dynamic")
+        {
+                $group = get-azureadmsgroup -all $true |
+                ?{$_.grouptypes -contains "DynamicMembership"}|
+                select displayname,description, id, securityenabled, IsAssignableToRole, proxyaddresses, GroupTypes,  MailEnabled, Mail, mailnickname,AssignedLabels, MembershipRule |
+                Sort-Object DisplayName
+
+        }
+        elseif($GroupTypeQuestion-eq "Assigned")
+        {
+                $group = get-azureadmsgroup -all $true |
+                ?{$_.grouptypes -notcontains "DynamicMembership"}|
+                select displayname,description, id, securityenabled, IsAssignableToRole, proxyaddresses, GroupTypes,  MailEnabled, Mail, mailnickname,AssignedLabels, MembershipRule |
+                Sort-Object DisplayName
+            
+        }
+
+        else # condition for all Group Attributes, all a&d and all s/o
+        { 
+            $group = get-azureadmsgroup -all $true |
+                select displayname,description, id, securityenabled, IsAssignableToRole, proxyaddresses, GroupTypes,  MailEnabled, Mail, mailnickname,AssignedLabels, MembershipRule |
+                Sort-Object DisplayName 
+        }
+        if ($group -eq 0)
+        {
+                        $groupinfo2 = "No groups"
+                        $groupinfo += $groupinfo2
+        }
+        else
+        {
+            foreach ($item in $group)
+            {
+
+                        $groupinfo2 = $item.DisplayName +","+ $item.Description +"," + $item.Id + ","+ $item.securityenabled+ ","+ $item.IsAssignableToRole + "," +  $item.proxyaddresses + "," +  $item.GroupTypes + "," +  $item.MailEnabled+ "," +  $item.Mail+ "," +  $item.mailnickname+ "," +  $item.AssignedLabels+ "," +  $item.MembershipRule
+                        $groupinfo += $groupinfo2
+            }
+        }
+        #creating header for CSV
+        $groupoutput = "Group_DisplayName,Group_Description,Id,securityenabled,IsAssignableToRole ,proxyaddresses ,GroupTypes ,MailEnabled,Mail,mailnickname,AssignedLabels,MembershipRule"
+        #$file = $MainMenuQuestion +"_"+ $GroupTypeQuestion+"_"+ $SorOQuestion
+        #$OutputFile = save-file -filename $file -initialDirectory $env:HOMEDRIVE
+        #if ($OutputFile -eq "Cancel"){break}
+        $groupoutput |  Out-File -FilePath $outputfile -Encoding utf8 -Force
+        #adding info to CSV
+        $groupinfo | Out-File -FilePath $outputfile -Encoding utf8 -Append
+    }
+
+
---
+++ UI Export RBAC V6.ps1
@@ -0,0 +1,290 @@
+﻿#######################
+# the below script uses the AZ powershell module pulling all roleassignments for groups
+# V2 added in sorting selection by DisplayName, RoleDefinitionName and Scope
+# V2 changed select-list box function name to more appropriate
+# ALL RBAC Version
+# 2/17/2022 
+# V6
+# updated to select multiple tenant and subscriptions then iterate through all selected
+# removed unused functions and user/group/spn selection options for now
+# written by debaxter@microsoft.com
+#######################
+
+$WarningPreference = "SilentlyContinue"
+#function for picking a directory to save the file
+function save-directory([string] $initialDirectory, $filename)
+    {
+        #$outputfile = $filename
+        #$SaveFileDialog = New-Object windows.forms.savefiledialog  
+        #$SaveFileDialog.FileName = $filename 
+        #$SaveFileDialog.initialDirectory = $initialDirectory
+        #$SaveFileDialog.title = "Select directory"
+        #$result = $SaveFileDialog.ShowDialog((New-Object System.Windows.Forms.Form -Property @{TopMost = $true; TopLevel = $true}))
+  
+        $dialog = New-Object System.Windows.Forms.FolderBrowserDialog
+        if ($dialog.ShowDialog((New-Object System.Windows.Forms.Form -Property @{TopMost = $true; TopLevel = $true})) -eq [System.Windows.Forms.DialogResult]::OK) 
+        {
+            $directoryName = $dialog.SelectedPath
+            Write-Host "Directory selected is $directoryName"
+        }
+        
+
+        return $directoryName
+    } 
+
+#function for creating list boxes
+function select-listbox ($grouptype, $selectType, $groupselect, $multivalue, $scopelist )
+    {
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-Type -AssemblyName System.Drawing
+
+    $form = New-Object System.Windows.Forms.Form
+    $form.Text = 'Select ' + $selectType
+    $form.Size = New-Object System.Drawing.Size(400,300)
+    $form.StartPosition = 'CenterScreen'
+    $form.Top = $true
+    $form.TopMost = $true
+
+    $okButton = New-Object System.Windows.Forms.Button
+    $okButton.Location = New-Object System.Drawing.Point(200,230)
+    $okButton.Size = New-Object System.Drawing.Size(75,23)
+    $okButton.Text = 'OK'
+    $okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+    $form.AcceptButton = $okButton
+    $form.Controls.Add($okButton)
+
+    $cancelButton = New-Object System.Windows.Forms.Button
+    $cancelButton.Location = New-Object System.Drawing.Point(275,230)
+    $cancelButton.Size = New-Object System.Drawing.Size(75,23)
+    $cancelButton.Text = 'Cancel'
+    $cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+    $form.CancelButton = $cancelButton
+    $form.Controls.Add($cancelButton)
+
+    $label = New-Object System.Windows.Forms.Label
+    $label.Location = New-Object System.Drawing.Point(10,20)
+    $label.Size = New-Object System.Drawing.Size(365,20)
+    $label.Text = 'Please select ' + $selectType
+    $form.Controls.Add($label)
+
+
+    $listBox = New-Object System.Windows.Forms.ListBox
+    $listBox.Location = New-Object System.Drawing.Point(10,50)
+    $listBox.Size = New-Object System.Drawing.Size(365,20)
+    $listBox.Height = 180
+    
+    if($multivalue)
+        {
+            $listBox.SelectionMode = 'MultiExtended'
+        }
+
+    foreach ($item in $scopelist)
+        {
+            [void] $listBox.Items.Add($item)
+        }
+
+    foreach ($item in $grouptype)
+        {
+            [void] $listBox.Items.Add($item)
+        }
+    foreach ($item in $groupselect)
+        {
+            [void] $listBox.Items.Add($item)
+        }
+
+
+    $form.Controls.Add($listBox)
+    $form.Topmost = $true
+    $result = $form.ShowDialog()
+    if ($result -eq [System.Windows.Forms.DialogResult]::OK)
+        {
+            $x = $listBox.SelectedItems
+            #$x
+        }
+    elseif ($result -eq [System.Windows.Forms.DialogResult]::Cancel)
+        {
+            
+            break
+        }
+    return $x
+    } # end function select-app
+
+    $audittype = @()
+    $aduserquestion =@()
+    $adusers =@()
+    $fileprefix =@()
+    $roleinfo =@()
+    $roleinfo2 =@()
+#function to build the csv file and save
+function build-file ($anotherlist, $scopetype, $filename)
+{
+        $roleinfo =@()
+        $roleinfo2=@()
+         if ($selectscope -eq "Cancel"){break}
+        $rbacrolelist = $list |Sort-Object $scopetype 
+        
+            if ($rbacrolelist -eq $null)
+            { 
+                break
+            }
+            else
+                {
+            
+                    foreach ($item3 in $rbacrolelist)
+                    {
+                        $roleinfo2 = $item3.DisplayName  +","+  $item3.ObjectID+","+  $item3.ObjectType+","+  $item3.RoleDefinitionName+","+  $item3.Scope+","+  $item3.RoleDefinitionId +","+  $item3.RoleAssignmentId
+                        $roleinfo += $roleinfo2
+                    }
+                }
+        $tdy = get-date -Format "MM-dd-yyyy hh.mm.ss"
+        $filename1 = $filename +" " + $subscriptionselected.name+" "+$subscriptionselected.id +" "+ $domainselected +" "+ $tdy +".csv"           
+        $OutputFile = "$outputdirectory"+"\"+"$filename1"
+        $AASRolegroupoutput = "'DisplayName','ObjectID','ObjectType','RoleDefinitionName','Scope','RoleDefinitionID','RoleAssignmentID'"
+        $AASRolegroupoutput |  Out-File -FilePath $OutputFile -Encoding utf8 -Force
+        $roleinfo | Out-File -FilePath $OutputFile -Encoding utf8 -Append
+
+}
+
+#logging into az
+try
+{
+    get-azaduser -ErrorAction stop >$null
+}
+catch
+{
+    $firstlogin = Connect-AzAccount
+}
+
+
+
+$AASRolegroupoutput =@()
+
+if ($OutputFile -eq "Cancel"){break}
+
+
+#Getting all domains in Azcontext after login : Making sure any tenants that need auth are prompted
+$getdomains = (get-azdomain).name
+
+$domainquestion =  select-listbox -grouptype $getdomains -selectType "Multiselect Domain Filter"  -multivalue $true
+if ($domainquestion -eq "Cancel"){break}
+
+$othertenants =@()
+foreach ($selecteddomain in $domainquestion)
+{
+$othertenants += get-azdomain | ?{$_.id -ne $firstlogin.Context.Tenant.id -and $_.Name -eq $selecteddomain}
+}
+
+foreach ($domainitem in $othertenants )
+{
+    write-host "Adding Tenant : " $domainitem.Id " Tenant Name : " $domainitem.Name
+    Add-AzAccount -Tenant $domainitem.id  
+}
+$getsubscriptions =@()
+#getting all subscriptions for domains selected
+foreach ($domainselected in $domainquestion)
+{
+    $domainid = (get-azdomain |?{$_.Name -eq $domainselected}).Id
+    $getsubscriptions += (Get-AzSubscription -TenantId $domainid).Name
+}
+$subsxquestion =  select-listbox -grouptype $getsubscriptions -selectType "Multiselect Subscription filter"  -multivalue $true
+if ($subsxquestion -eq "Cancel"){break}
+
+$subs =@()
+Foreach ($subanswer in $subsxquestion)
+{
+    $subs += Get-AzSubscription |?{$_.Name -eq "$subanswer"}
+}
+
+
+$counter = 0
+$outputdirectory = save-directory
+$groupquestion=@()
+
+$grouptype = @("All RBAC","All Users","All Groups", "All Service Principals", "Identity Unknown")
+$groupquestion =  select-listbox -grouptype $grouptype -selectType $RBACSelecttype
+if ($groupquestion -eq "Cancel"){break}    
+
+$RBACSelecttype = "RBAC Filter option: Domain $domainselected"
+$scopetype = @("DisplayName","Scope","RoleDefinitionName")
+$selectscope = select-listbox -scopelist $scopetype -selectType "Sort by Filter"
+if ($selectscope -eq "Cancel"){break}
+
+ 
+
+    foreach ($subscriptionselected in $subs)
+        {
+            $list =@()
+            $list2 =@()
+            
+            $file =@()
+            $outputfile =@()
+            $rbacrole =@()
+            $rbaclist =@()
+            $rbacrolelist =@()
+            $roleinfo =@()
+            $roleinfo2 =@()
+            $item =@()
+            $item2 =@()
+            $item3 =@()
+            $AASRolegroupoutput =@()
+            $scopetype =@()
+            $scopeselect =@()
+
+            $file = $groupquestion
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+                    write-host "Scanning RBAC Roles in Subscription : " $subscriptionselected.Name
+                    Set-AzContext -Subscription $subscriptionselected.id #-Tenant $subselectect.TenantId
+
+                if ($groupquestion -eq "All RBAC")
+                {                
+                    
+                    $list = get-azroleassignment  
+                    $users = get-azroleassignment # | Where-Object {$_.SignInName -ne $null} 
+                    foreach ($userfound in $users)
+                    {
+                        $list += $userfound
+                    }
+
+                    build-file -anotherlist $list -filename $file  -scopetype $selectscope -outputdirectory $outputdirectory
+                }
+
+                elseif ($groupquestion -eq "All Groups")
+                {
+                    $objecttype = "Group"
+
+                    $list = get-azroleassignment | Where-Object {$_.ObjectType -eq $objecttype}  #|select DisplayName, RoleAssignmentId, Scope, RoleDefinitionName, RoleDefinitionId, ObjectID, ObjectType |Sort-Object DisplayName, RoleDefinitionName
+                    build-file -passlist $list -filename $file  -scopetype $selectscope -outputdirectory $outputdirectory
+                }
+                # gets all User role assignments
+                elseif ($groupquestion -eq "All Users")
+                {
+                    $objecttype = "User"
+                    $list = get-azroleassignment  | Where-Object {$_.ObjectType -eq $objecttype -and $_.SignInName -ne $null} 
+                    build-file -passlist $list -filename $file -scopetype $selectscope -outputdirectory $outputdirectory
+                }
+                # gets all ServicePrincipal role assignments
+                elseif ($groupquestion -eq "All Service Principals")
+                {
+                    $objecttype = "ServicePrincipal"
+                    $list = get-azroleassignment  | Where-Object {$_.ObjectType -eq $objecttype} 
+                    build-file -passlist $list -filename $file  -scopetype $selectscope -outputdirectory $outputdirectory
+                }
+                # gets all role assignments
+
+                # gets all Identity Unknown role assignments - Either User, Group, SPN was deleted
+                elseif ($groupquestion -eq "Identity Unknown")
+                {
+        
+                    $objecttype = "Unknown"
+                    
+                    $list = get-azroleassignment  | Where-Object {$_.displayname -eq $null -and $_.SignInName -eq $null}
+                    build-file -passlist $list -filename $file  -scopetype $selectscope -outputdirectory $outputdirectory
+                }
+
+        }
+    
+    #}
+    
+
+    

75ad0fcf1e75db3ef7d29f1d4c17597bf7d01e68
debaxtermsft <92338056+debaxtermsft@users.noreply.github.com>
2022-04-25 13:06:11 -0400

    Add files via upload
    How to create a new application and service principal for Azure AD using MG Graph module
    Then have that service principal login to manage groups

---
+++ MG Graph New App SPN Group Management Script.ps1
@@ -0,0 +1,58 @@
+﻿# Created 4/25/2022
+# written by Debaxter
+# how to create a new application and service principal to be able to login and manage group memberships
+# using azure mg graph api
+
+connect-mggraph -Scopes "Directory.ReadWrite.All, Application.Read.All, Application.ReadWrite.All, RoleManagement.ReadWrite.Directory"
+
+New-mgApplication -DisplayName "MGAddGroupSPN" -IdentifierUris https://mgspn.domain.space
+$MyApp = Get-MgApplication -Filter "Displayname eq 'MGAddGroupSPN'"
+$myapp | select Id, appid
+
+#Id                                   AppId
+#--                                   -----
+#d3382446-xxxx-yyyy-zzzz-aaaabbbbcccc 719520c8- xxxx-yyyy-zzzz-aaaabbbbcccc
+
+New-MgServicePrincipal -appid $myapp.AppId
+$SPNID = Get-MgServicePrincipal -Filter "DisplayName eq 'MGAddGroupSPN'"
+$spnid | select id, appid, displayname
+
+#Id                                   AppId                                DisplayName
+#--                                   -----                                -----------
+#a4cc78cf-xxxx-yyyy-zzzz-aaaabbbbcccc 719520c8-xxxx-yyyy-zzzz-aaaabbbbcccc MGAddGroupSPN
+      
+Assigning the “Directory Reader Role” to the new service principal
+NOTE! This is a MUST
+#Assigning the “Directory Reader Role” to the new service principal
+$DRoID = Get-MgDirectoryRole | ?{$_.DisplayName -match "Directory Reader"}
+
+#add directory reader role to SPN
+
+#User the applications objectid
+$DirObject = @{
+  "@odata.id" = https://graph.microsoft.com/v1.0/directoryObjects/a4cc78cf-xxxx-yyyy-zzzz-aaaabbbbcccc
+  }
+
+New-MgDirectoryRoleMemberByRef -DirectoryRoleId $DRoID.Id -BodyParameter $DirObject
+
+#(Add the API permissions and certificate to the new service principal)
+ 
+#AzureAD portal\apps registration\All and locate the app - Application
+# Api permissions + Add Permission \ Graph \ Application Permissions
+# Directory.ReadWrite.All
+# Group.Read.All
+# Group.ReadWrite.All
+# GroupMember.Read.All
+# GroupMember.ReadWrite.All
+#
+# Under Certificate - Add the PEM certificate for the service principal 
+
+# Within your script
+
+Connect-MgGraph -ClientId "719520c8-xxxx-yyyy-zzzz-aaaabbbbcccc" -TenantId "7b69d5f1-xxxx-yyyy-zzzz-aaaabbbbcccc " -CertificateThumbprint "Thumbprint" 
+
+# To add a user to a group
+new-mggroupmember -GroupId (Get-MgGroup -Filter "Displayname eq 'MyGroup'").id -DirectoryObjectId (Get-MgUser -Filter "Displayname eq 'First Last'").Id 
+
+# To add a service principal
+new-mggroupmember -GroupId (Get-MgGroup -Filter "Displayname eq 'MyGroup'").id -DirectoryObjectId (Get-MgServicePrincipal -Filter "Displayname eq 'OtherSPN'").Id 

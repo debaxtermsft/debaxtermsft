a562541364706c5cfd5d1dd04660e2c951ebac63
debaxtermsft <debaxter@microsoft.com>
2022-08-01 18:13:07 -0400

    Update updated_tls_script.ps1

--- updated_tls_script.ps1
+++ updated_tls_script.ps1
@@ -1,20 +1,21 @@
 $tId = "7b69d5f1-a08a-4802-9c12-794ca524d88d"  # Add tenant ID from Azure Active Directory page on portal.
 $tdy = get-date -Format "MM-dd-yyyy hh.mm.ss"
-$agoDays = 4  # Will filter the log for $agoDays from the current date and time.
+$agoDays = 30  # Will filter the log for $agoDays from the current date and time.
 $startDate = (Get-Date).AddDays(-($agoDays)).ToString('yyyy-MM-dd')  # Get filter start date.
 $pathForExport = "c:\temp\"  # The path to the local filesystem for export of the CSV file.
 
-Connect-MgGraph -Scopes "AuditLog.Read.All" -TenantId $tId  # Or use Directory.Read.All.
+Connect-MgGraph -Scopes "AuditLog.Read.All, directory.read.all" -TenantId $tId  # Or use Directory.Read.All.
 Select-MgProfile "beta"  # Low TLS is available in Microsoft Graph preview endpoint.
 
 # Define the filtering strings for interactive and non-interactive sign-ins.
 $procDetailFunction = "x: x/key eq 'legacy tls (tls 1.0, 1.1, 3des)' and x/value eq '1'"
 
 $SIIProperties =@()
+$SINIProperties =@()
 $SIWIProperties =@()
 
 # Get the interactive and non-interactive sign-ins based on filtering clauses.
-$legacy = Get-MgAuditLogSignIn -Filter "createdDateTime ge $startDate"  | 
+$legacy = Get-MgAuditLogSignIn -all -Filter "createdDateTime ge $startDate"  | 
 Where-Object{$_.AuthenticationProcessingDetails.key -match "Legacy"}  | sort-object CreatedDateTime
 write-host "SII"
 if ($legacy.count -ge 1)
@@ -58,14 +59,60 @@ if ($legacy.count -ge 1)
                 
             }
              
-            Start-Sleep 1
+            #Start-Sleep 1
     }
 
     
 }
+write-host "SINI"
+if ($legacy.count -ge 1)
+{
+    $filterUsernames = $legacy | Select-Object userprincipalname | sort-object -Unique  userprincipalname
+    foreach ($useritem in $filterUsernames)
+    {
+        $username = $useritem.userprincipalname 
+        $clauses = (
+            "createdDateTime ge $startDate",
+            "signInEventTypes/any(t: t eq 'nonInteractiveUser')",
+            "signInEventTypes/any(t: t eq 'servicePrincipal')",
+            "userPrincipalName eq '$username'"
+        )
+        $SINIfilter = $clauses[0,1,3] -join " and "
+        
+        $signInsNInteractive = Get-MgAuditLogSignIn -Filter $SINIfilter -All  | 
+        Where-Object{$_.AuthenticationProcessingDetails.key -match "Legacy" -and $_.AuthenticationProcessingDetails.Value -eq "True"}
+        write-host "Found Legacy TLS login for user " $useritem.userprincipalname
+            foreach ($SINILog in $signInsNInteractive)
+            {
+                $userauthprocdetails0 = $SINILog.AuthenticationProcessingDetails.key[0]
+                $userauthprocdetails1 = $SINILog.AuthenticationProcessingDetails.key[1]
+            $SINIProperties += New-Object Object |
+                Add-Member -NotePropertyName CorrelationId -NotePropertyValue $SINILog.CorrelationId -PassThru |
+                Add-Member -NotePropertyName createdDateTime -NotePropertyValue $SINILog.createdDateTime -PassThru |
+                Add-Member -NotePropertyName userPrincipalName -NotePropertyValue $SINILog.userprincipalname -PassThru |
+                Add-Member -NotePropertyName userId -NotePropertyValue $SINILog.userId -PassThru |
+                Add-Member -NotePropertyName UserDisplayName -NotePropertyValue $SINILog.UserDisplayName -PassThru |
+                Add-Member -NotePropertyName AppDisplayName -NotePropertyValue $SINILog.appdisplayname -PassThru |
+                Add-Member -NotePropertyName AppId -NotePropertyValue $SINILog.Appid -PassThru |
+                Add-Member -NotePropertyName IPAddress -NotePropertyValue $SINILog.IPAddress -PassThru |
+                Add-Member -NotePropertyName isInteractive -NotePropertyValue $SINILog.isInteractive -PassThru |
+                Add-Member -NotePropertyName ResourceDisplayName -NotePropertyValue $SINILog.ResourceDisplayName -PassThru |
+                Add-Member -NotePropertyName ResourceServicePrincipal -NotePropertyValue $SINILog.ResourceServicePrincipalId -PassThru |
+                Add-Member -NotePropertyName ResourceId -NotePropertyValue $SINILog.ResourceId -PassThru |
+                Add-Member -NotePropertyName UserAgent -NotePropertyValue $SINILog.UserAgent -PassThru |
+                Add-Member -NotePropertyName AuthenticationProcessingDetails0 -NotePropertyValue $userauthprocdetails0 -PassThru |
+                Add-Member -NotePropertyName AuthenticationProcessingDetails1 -NotePropertyValue $userauthprocdetails1 -PassThru |
+                Add-Member -NotePropertyName authenticationRequirement -NotePropertyValue $SINILog.authenticationRequirement -PassThru
+                
+            }
+             
+            # Start-Sleep 1
+    }
 
+    
+}
 $MGserviceprincipals = @()
-$checkSPNlegacylogins = Get-MgAuditLogSignIn -Filter "createdDateTime ge $startDate and signInEventTypes/any(t: t eq 'servicePrincipal')" | 
+$checkSPNlegacylogins = Get-MgAuditLogSignIn -all -Filter "createdDateTime ge $startDate and signInEventTypes/any(t: t eq 'servicePrincipal')" | 
 sort-object CreatedDateTime  | 
 Where-Object{$_.AuthenticationProcessingDetails.key -match "Legacy"}
 write-host "SIWI"
@@ -108,9 +155,10 @@ if($checkSPNlegacylogins.count -ge 1)
                     
                 }
               
-        Start-Sleep 1
+      #  Start-Sleep 1
     }
 }
 $SIIProperties | Export-Csv -Path ($pathForExport + "Interactive_lowTls_$tId_$tdy.csv") -NoTypeInformation
+$SINIProperties | Export-Csv -Path ($pathForExport + "NONInteractive_lowTls_$tId_$tdy.csv") -NoTypeInformation
 $SIWIProperties  | Export-Csv -Path ($pathForExport + "WorkloadIdentities_lowTls_$tId_$tdy.csv") -NoTypeInformation
 

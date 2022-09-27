a05519bbb5ed7bdaf3de042de60ed620ea6fbb9a
debaxtermsft <debaxter@microsoft.com>
2022-09-27 14:39:04 -0400

    Create AADDS DNS Records Export.ps1

---
+++ AADDS DNS Records Export.ps1
@@ -0,0 +1,36 @@
+<#
+Written by Derrick Baxter 9.27.2022
+Used to export DNS Records to a backup CSV file
+to recreate any lost records follow the document below
+https://learn.microsoft.com/en-us/powershell/module/dnsserver/add-dnsserverresourcerecord?view=windowsserver2022-ps
+#>
+
+param([parameter(mandatory)][string] $AADDS,
+      [parameter(mandatory)] [string]$DCIPAddress,
+      [parameter(mandatory)][validateset("Host Records","All")] [string]$recordtype,
+      [parameter(mandatory)] [string]$OutputFile)
+
+$WarningPreference = "SilentlyContinue"  
+
+import-module -name dnsserver
+
+if($recordtype -eq "All")
+{
+    $DNS = Get-DnsServerResourceRecord -zonename $AADDS -ComputerName $DCIPAddress
+}
+else {
+    $DNS = Get-DnsServerResourceRecord -zonename $AADDS -ComputerName $DCIPAddress -RRType A
+}
+$members =@()
+foreach($dnsrecorditem in $DNS)
+{
+    $IP = $dnsrecorditem.recorddata.ipv4address.IPAddressToString
+    $members += New-Object Object |
+                Add-Member -NotePropertyName HostName -NotePropertyValue $dnsrecorditem.HostName -PassThru |
+                Add-Member -NotePropertyName IPAddress -NotePropertyValue $IP -PassThru | 
+                Add-Member -NotePropertyName RecordType -NotePropertyValue $dnsrecorditem.RecordType -PassThru | 
+                Add-Member -NotePropertyName Type -NotePropertyValue $dnsrecorditem.Type -PassThru 
+
+}
+
+$members | export-csv -Path $OutputFile -NoTypeInformation -Encoding UTF8

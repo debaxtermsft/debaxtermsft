533370d08c9839e197e5cc9ca799dacb300c4369
debaxtermsft <debaxter@microsoft.com>
2023-04-17 14:53:41 -0400

    Update commercial_graphapi_signinactivitywPasswordLastChangeDateTime.ps1

--- commercial_graphapi_signinactivitywPasswordLastChangeDateTime.ps1
+++ commercial_graphapi_signinactivitywPasswordLastChangeDateTime.ps1
@@ -9,7 +9,7 @@ connect-mggraph -scopes "directory.read.all, auditlog.read.all, user.read.all" -
 
 
 #by UPN
-#$ApiUrl = "https://graph.microsoft.com/v1.0/users?`$filter=startswith(userprincipalname,'derrick.baxter@saviors18twd.onmicrosoft.com')&`$select=displayName,userprincipalname,signInActivity,lastPasswordChangeDateTime"
+#$ApiUrl = "https://graph.microsoft.com/v1.0/users?`$filter=startswith(userprincipalname,'derrick.baxter@domain.onmicrosoft.com')&`$select=displayName,userprincipalname,signInActivity,lastPasswordChangeDateTime"
 #by date
 #$ApiUrl = "https://graph.microsoft.com/v1.0/users?`$filter=signInActivity/lastSignInDateTime le 2023-04-11T00:00:00Z&`$select=displayName,userprincipalname,lastPasswordChangeDateTime,signInActivity"
 

f408127e766be88df90b63e33dd915491ce28d6a
debaxtermsft <debaxter@microsoft.com>
2023-05-15 14:29:08 -0400

    removed my tenant name

--- commercial_graphapi_signinactivity.ps1
+++ commercial_graphapi_signinactivity.ps1
@@ -9,7 +9,7 @@ connect-mggraph -scopes "directory.read.all, auditlog.read.all, user.read.all" -
 
 
 #by UPN
-#$ApiUrl = "https://graph.microsoft.com/v1.0/users?`$filter=startswith(userprincipalname,'derrick.baxter@saviors18twd.onmicrosoft.com')&`$select=displayName,userprincipalname,signInActivity"
+#$ApiUrl = "https://graph.microsoft.com/v1.0/users?`$filter=startswith(userprincipalname,'derrick.baxter@tenant.onmicrosoft.com')&`$select=displayName,userprincipalname,signInActivity"
 #by date
 #$ApiUrl = "https://graph.microsoft.com/v1.0/users?`$filter=signInActivity/lastSignInDateTime le 2023-04-11T00:00:00Z&`$select=displayName,userprincipalname,signInActivity"
 

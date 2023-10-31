a0327442ebe11c0e13a6d235ef1b4de01aa0d7fc
debaxtermsft <debaxter@microsoft.com>
2023-10-31 10:55:30 -0400

    Added consent script

--- AUGroupMemberBulkImport.ps1
+++ AUGroupMemberBulkImport.ps1
@@ -6,9 +6,29 @@ CSV created gm.csv saved in c:\temp
 groupmemberobjectid
 "6674bd59-d010-4a9c-b0ed-d2c3f786c677"
 "eb9f7358-6b92-4bc9-b2e4-699a9fa97185"
-
-
 .\augroupMemberImport.ps1 -importfile c:\temp\gm.csv -groupobjectid "b1f92b75-groupoid-guid"
+
+
+Each user will need a GA to run the below script to grant consent to Microsoft Graph
+#----------------------- consent script start ----------------
+$sp = get-mgserviceprincipal | ?{$_.displayname -eq "Microsoft Graph"}
+$resource = Get-MgServicePrincipal -Filter "appId eq '00000003-0000-0000-c000-000000000000'"
+$principalid = "users object id" # look up and add the users object id here between the quotes
+$scope1 ="directory.write.restricted"
+$scope2 ="directory.read.all"
+$scope3 ="group.read.all"
+$scope4 ="groupmember.readwrite.all"
+
+$params = @{
+    ClientId = $SP.Id
+    ConsentType = "Principal"
+    ResourceId = $resource.id
+    principalId = $principalid
+    Scope = "$scope1" + " " + "$scope2"+ " " + "$scope3"+ " " + "$scope4"
+}
+$InitialConsented = New-MgOauth2PermissionGrant -BodyParameter $params
+#----------------------- consent script end ----------------
+
 #>
 
 ---- AU bulk member import via powershell

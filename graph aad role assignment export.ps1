ee9d3b8e19151fde41c92c7547560b938c11fd8f
debaxtermsft <debaxter@microsoft.com>
2023-12-08 10:50:29 -0500

    updated version

--- graph aad role assignment export.ps1
+++ graph aad role assignment export.ps1
@@ -34,29 +34,42 @@ You may need to update the connect-mggraph to have the -environment USGov or as 
 #>
 
 Import-Module Microsoft.Graph.DeviceManagement.Enrollment
-function save-file([string] $initialDirectory, $filename)
-    {
-        $outputfile = $filename
-        $SaveFileDialog = New-Object windows.forms.savefiledialog  
-        $SaveFileDialog.FileName = $filename 
-        $SaveFileDialog.initialDirectory = $initialDirectory
-        $SaveFileDialog.title = "Save File to Disk"
-        $SaveFileDialog.filter = "AzureADRoleExports | AAD Roles Export*.csv|Comma Seperated File|*.csv | All Files|*.* " 
-        $SaveFileDialog.ShowHelp = $True   
+<#
+ the below script uses the AzureAD powershell module pulling all roleassignments for groups
+10/12/23
+# updates 
+# written by debaxter
 
-        $result = $SaveFileDialog.ShowDialog((New-Object System.Windows.Forms.Form -Property @{TopMost = $true; TopLevel = $true}))
-        
-        if($SaveFileDialog.Filenames -eq "" ) 
-            {
-             #   write-host "Its not Null" 
-                exit
-            } 
-        else
-            {
-                $x = $SaveFileDialog.Filename  
-            }
-        return $x
-    } 
+
+
+IMPORTANT!!!
+
+To consent for users to run this script a global admin will need to run the following
+-------------------------------------------------------------------------------------------
+$sp = get-mgserviceprincipal | ?{$_.displayname -eq "Microsoft Graph"}
+$resource = Get-MgServicePrincipal -Filter "appId eq '00000003-0000-0000-c000-000000000000'"
+$principalid = "users object id"
+$scope1 ="group.read.all"
+$scope2 ="directory.read.all"
+$scope3 ="groupmember.read.all"
+$scope4 ="Policy.Read.ConditionalAccess"
+$scope5 ="Application.Read.All"
+
+$params = @{
+    ClientId = $SP.Id
+    ConsentType = "Principal"
+    ResourceId = $resource.id
+    principalId = $principalid
+    Scope = "$scope1" + " " + "$scope2"+ " " + "$scope3"+ " " + "$scope4"+ " " + "$scope5"
+}
+
+$InitialConsented = New-MgOauth2PermissionGrant -BodyParameter $params
+-------------------------------------------------------------------------------------------
+You may need to update the connect-mggraph to have the -environment USGov or as needed -tenantid <tenantid> can be added as needed.
+
+#>
+
+Import-Module Microsoft.Graph.DeviceManagement.Enrollment
 
 
 try
@@ -70,10 +83,7 @@ catch
     }
 
         $members =@()
-        #foreach ($item in $aadrole)
-        #{
             $filter1 = "roleDefinitionId eq "+"'"+$item.Id+"'"
-            #$aadrolemember = Get-MgRoleManagementDirectoryRoleAssignment -Filter "$filter1" 
             $aadrolemember = Get-MgRoleManagementDirectoryRoleAssignment -all | Sort-Object RoleDefinitionId
             write-host "Role assignment count : " $aadrolemember.count
            if ($aadrolemember -ne $null)
@@ -127,8 +137,8 @@ catch
         #return $members
         $tdy = get-date -Format "MM-dd-yyyy hh.mm.ss"
         $outputfile = "AADRoleAssignmentExport_"+$tdy+".csv"
-        $OutputFile = save-file -filename $outputfile -initialDirectory $env:HOMEDRIVE
+        $OutputFile2 = "c:\temp\"+$outputfile
         if ($OutputFile -eq "Cancel"){break}
-        $members  | Sort-Object AADRoleName, DisplayName | export-csv -Path $OutputFile -NoTypeInformation -Encoding utf8 -Force
+        $members  | Sort-Object AADRoleName, DisplayName | export-csv -Path "$OutputFile2" -NoTypeInformation -Encoding utf8 -Force
 
     

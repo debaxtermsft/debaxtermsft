0c3de76514fe9f49365c60af637b05b402922114
debaxtermsft <debaxter@microsoft.com>
2024-01-04 15:27:11 -0500

    Added OU Inheritance Checker
    OU Inheritance Check
    Entire Forest
    .\EntraIDConnectPermissionsInheritanceChecker.ps1 -PermissionsCheck Inheritance -outputfile output1424 -OUorUser OU
    or
    .\EntraIDConnectPermissionsInheritanceChecker.ps1 -dn "DC=synclab,DC=local" -PermissionsCheck Inheritance -outputfile output1424 -OUorUser OU
    Specific OU (sublevels)
    .\EntraIDConnectPermissionsInheritanceChecker.ps1 -dn "OU=corp,DC=synclab,DC=local" -PermissionsCheck Inheritance -outputfile output1424 -OUorUser OU

--- EntraIDConnectPermissionsInheritanceChecker.ps1
+++ EntraIDConnectPermissionsInheritanceChecker.ps1
@@ -31,16 +31,23 @@ DN :  CN=R2 D3,OU=users,OU=Sales,OU=corp,DC=synclab,DC=local
 
   By OU level to pull all users under an OU for Entra Connect Permissions
  Searching on a user to see if they have the correct permissions for Entra Connect
- .\EntraIDConnectPermissionsInheritanceChecker.ps1.ps1 -dn "OU=corp,DC=synclab,DC=local" -PermissionsCheck 'Entra Connect Service Account' -outputfile output.csv -adcserviceprincipal MSOL_xxxxyyyyzzzz
+ .\EntraIDConnectPermissionsInheritanceChecker.ps1 -dn "OU=corp,DC=synclab,DC=local" -PermissionsCheck 'Entra Connect Service Account' -outputfile output.csv -adcserviceprincipal MSOL_xxxxyyyyzzzz
  
  Searching on a user to see if they have the correct permissions for Inheritance Disabled
-.\EntraIDConnectPermissionsInheritanceChecker.ps1.ps1 -dn "OU=corp,DC=synclab,DC=local" -PermissionsCheck Inheritance -outputfile output.csv -adcserviceprincipal MSOL_xxxxyyyyzzzz
+.\EntraIDConnectPermissionsInheritanceChecker.ps1 -dn "OU=corp,DC=synclab,DC=local" -PermissionsCheck Inheritance -outputfile output.csv -adcserviceprincipal MSOL_xxxxyyyyzzzz
 Unsure of the MSOL full account name - just use MSOL or NOTHING
-.\EntraIDConnectPermissionsInheritanceChecker.ps1.ps1 -dn "OU=corp,DC=synclab,DC=local" -PermissionsCheck Inheritance -outputfile output.csv -adcserviceprincipal MSOL
-.\EntraIDConnectPermissionsInheritanceChecker.ps1.ps1 -dn "OU=corp,DC=synclab,DC=local" -PermissionsCheck Inheritance -outputfile output.csv 
+.\EntraIDConnectPermissionsInheritanceChecker.ps1 -dn "OU=corp,DC=synclab,DC=local" -PermissionsCheck Inheritance -outputfile output.csv -adcserviceprincipal MSOL
+.\EntraIDConnectPermissionsInheritanceChecker.ps1 -dn "OU=corp,DC=synclab,DC=local" -PermissionsCheck Inheritance -outputfile output.csv 
+
+OU Inheritance Check
+Entire Forest
+.\EntraIDConnectPermissionsInheritanceChecker.ps1 -PermissionsCheck Inheritance -outputfile output1424 -OUorUser OU
+or
+.\EntraIDConnectPermissionsInheritanceChecker.ps1 -dn "DC=synclab,DC=local" -PermissionsCheck Inheritance -outputfile output1424 -OUorUser OU
+Specific OU (sublevels)
+.\EntraIDConnectPermissionsInheritanceChecker.ps1 -dn "OU=corp,DC=synclab,DC=local" -PermissionsCheck Inheritance -outputfile output1424 -OUorUser OU
 
 #>
-
 
 param([Parameter(Mandatory=$False)][string]$dn,
       [parameter(Mandatory)][validateset("OU","User")][string]$OUorUser,  
@@ -80,75 +87,89 @@ if($OUorUser -eq "User") #User Selected
         }
         $filename2 = $OUorUser+$PermissionsCheck+"_Check_"+ $outputfile + "_"+$tdy +".csv"
         $exportfile = $exportinheritancecheck | Where-Object{$_.inheritancedisabled -eq "True"} 
-        $exportfile | ft
         $exportfile  | export-csv -path $filename2 -NoTypeInformation -Force -Encoding UTF8
     }
     else #"Entra Connect Service Account" selected
     {
         [string]$holduser
-        #if($UserorGroup -eq "User")
-            #{
-                $foundacl = @()
-                foreach ($user in $adusers)
-                    {
-                    [string]$holduser = $user
-                    #$acls = & dsacls $holduser | findstr "$adcserviceprincipal" 
-                    $acls = & dsacls $holduser | select-string "$adcserviceprincipal" 
-                    foreach ($foundacl in $acls)
+        $foundacl = @()
+        foreach ($user in $adusers)
+            {
+            [string]$holduser = $user
+            $acls = & dsacls $holduser | select-string "$adcserviceprincipal" 
+            foreach ($foundacl in $acls)
+                {
+                    if($foundacl -match "pwdLastSet" -or $foundacl -match"lockoutTime" -or $foundacl -match"Change Password"-or $foundacl -match"Reset Password" -or $foundacl -match "mS-DS-ConsistencyGuid" -or  $foundacl -match "msDS-KeyCredential")
                         {
-                            if($foundacl -match "pwdLastSet" -or $foundacl -match"lockoutTime" -or $foundacl -match"Change Password"-or $foundacl -match"Reset Password" -or $foundacl -match "mS-DS-ConsistencyGuid" -or  $foundacl -match "msDS-KeyCredential")
-                                {
-                                    $exportacls += New-Object object |
-                                        Add-Member -NotePropertyName User -NotePropertyValue $user -PassThru |
-                                        Add-Member -NotePropertyName ACL -NotePropertyValue $foundacl -PassThru
-                                }
+                            $exportacls += New-Object object |
+                                Add-Member -NotePropertyName User -NotePropertyValue $user -PassThru |
+                                Add-Member -NotePropertyName ACL -NotePropertyValue $foundacl -PassThru
                         }
-                    }
-           # }
-            #$exportacls | Sort-Object User, missingACL | ft
+                }
+            }
         $filename = $OUorUser+$PermissionsCheck+ $outputfile + "_"+ $tdy +".csv"
-        $exportacls | ft
         $exportacls | export-csv -path $Filename -NoTypeInformation -Force
     }
 }
 else # "OU" selected
 {
-    $checkOUPerm = dsacls  $dn | select-string $adcserviceprincipal
-    #$checkOUPerm
-    write-host "In forest check"
-    write-host "Checking : " $dn 
-
-    $foundForestDN = @()
-    $foundOU = @()
-    foreach($OUItem in $checkOUPerm)
+    $exportinheritancecheck = @()
+    if($PermissionsCheck -eq "Inheritance")
     {
-        if($OUItem -match "pwdLastSet" -or $OUItem -match"lockoutTime" -or $OUItem -match"Change Password"-or $OUItem -match"Reset Password" -or $OUItem -match "mS-DS-ConsistencyGuid" -or  $OUItem -match "msDS-KeyCredential")
-            {
-              $foundOU += New-Object object |
-                Add-Member -NotePropertyName DistinguishedName -NotePropertyValue $dn -PassThru |
-                Add-Member -NotePropertyName ACL -NotePropertyValue $ouitem -PassThru
-            }
+        $orgunit = Get-ADOrganizationalUnit -filter *  -Properties nTSecurityDescriptor -SearchBase $dn
+        foreach ($item in $orgunit) 
+        {
+            
+            $orgsec = $item.ntsecuritydescriptor.AreAccessRulesProtected
+            $exportinheritancecheck += New-Object object |
+                Add-Member -NotePropertyName DN -NotePropertyValue $item.DistinguishedName -PassThru |
+                Add-Member -NotePropertyName InheritanceDisabled -NotePropertyValue $orgsec -PassThru
+        }
+            $exportinheritancecheck
+        $filename2 = $OUorUser+$PermissionsCheck+"_Check_"+ $outputfile + "_"+$tdy +".csv"
+        $exportfile = $exportinheritancecheck | Where-Object{$_.inheritancedisabled -eq "True"} 
+        #$exportfile | ft
+        $exportfile  | export-csv -path $filename2 -NoTypeInformation -Force -Encoding UTF8
     }
-
-    $allOUs = Get-ADOrganizationalUnit -Filter * | select distinguishedname
-    write-host "In OU check"
-    foreach($OUFound in $allOUs)
+    else # checking MSOL permissions"
     {
+        $checkOUPerm = dsacls  $dn | select-string $adcserviceprincipal
+        #$checkOUPerm
+        write-host "In forest check"
+        write-host "Checking : " $dn 
 
-
-        write-host "Checking OU : " $oufound.distinguishedname
-        $findOUACLs = dsacls $OUFound.distinguishedname | select-string $adcserviceprincipal    
-        foreach($OUItem in $findOUACLs)
+        $foundForestDN = @()
+        $foundOU = @()
+        foreach($OUItem in $checkOUPerm)
         {
             if($OUItem -match "pwdLastSet" -or $OUItem -match"lockoutTime" -or $OUItem -match"Change Password"-or $OUItem -match"Reset Password" -or $OUItem -match "mS-DS-ConsistencyGuid" -or  $OUItem -match "msDS-KeyCredential")
                 {
-                    $foundOU += New-Object object |
-                    Add-Member -NotePropertyName DistinguishedName -NotePropertyValue $OUFound.distinguishedname -PassThru |
-                    Add-Member -NotePropertyName ACL -NotePropertyValue $OUItem -PassThru
+                  $foundOU += New-Object object |
+                    Add-Member -NotePropertyName DistinguishedName -NotePropertyValue $dn -PassThru |
+                    Add-Member -NotePropertyName ACL -NotePropertyValue $ouitem -PassThru
                 }
         }
+
+        $allOUs = Get-ADOrganizationalUnit -Filter * | select distinguishedname
+        write-host "In OU check"
+        foreach($OUFound in $allOUs)
+        {
+
+
+            write-host "Checking OU : " $oufound.distinguishedname
+            $findOUACLs = dsacls $OUFound.distinguishedname | select-string $adcserviceprincipal    
+            foreach($OUItem in $findOUACLs)
+            {
+                if($OUItem -match "pwdLastSet" -or $OUItem -match"lockoutTime" -or $OUItem -match"Change Password"-or $OUItem -match"Reset Password" -or $OUItem -match "mS-DS-ConsistencyGuid" -or  $OUItem -match "msDS-KeyCredential")
+                    {
+                        $foundOU += New-Object object |
+                        Add-Member -NotePropertyName DistinguishedName -NotePropertyValue $OUFound.distinguishedname -PassThru |
+                        Add-Member -NotePropertyName ACL -NotePropertyValue $OUItem -PassThru
+                    }
+            }
+        }
+        $filename2 = "OU_Check_"+ $outputfile + "_"+$tdy +".csv"
+        $exportfoundOUs = $foundOU | ?{$_.ACL -notmatch "<Inherited from parent>"}
+        $exportfoundOUs| export-csv -Path $filename2 -NoTypeInformation -Force -Encoding UTF8
     }
-    $filename2 = "OU_Check_"+ $outputfile + "_"+$tdy +".csv"
-    $exportfoundOUs = $foundOU | ?{$_.ACL -notmatch "<Inherited from parent>"}
-    $exportfoundOUs| export-csv -Path $filename2 -NoTypeInformation -Force -Encoding UTF8
 }

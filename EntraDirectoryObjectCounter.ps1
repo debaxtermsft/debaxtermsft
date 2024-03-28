b66574b6f21d2f98749b001bbf489649064009fb
debaxtermsft <debaxter@microsoft.com>
2024-03-28 13:35:08 -0400

    cleaning up code

--- EntraDirectoryObjectCounter.ps1
+++ EntraDirectoryObjectCounter.ps1
@@ -1,6 +1,7 @@
 <# 
 Written by Derrick Baxter
 3/14/24
+3/28/24 Updated / Cleaned up code
 Retrieves directory object count/quota and object counts of users, groups, devices, applications and service principals
 pick the signin activity below (unrem) and run
 Update the $outputfile location from c:\temp\name as wanted - date and .csv are automatically added to avoid overwritting  
@@ -57,7 +58,7 @@ You may need to update the connect-mggraph to have the -environment USGov or as 
 
 #>
 param([parameter(Position=0,mandatory)][string]$Outputdirectory)
-
+#testing if user is already logged into graph
 try
     {
     Get-MGDomain -ErrorAction Stop > $null
@@ -66,50 +67,46 @@ catch
     {
     Connect-MgGraph -Scopes "directory.read.all"
     }
+# Retrieving Org Quota
+$org                = get-mgorganization | select -ExpandProperty AdditionalProperties
+$total              = $org.directorySizeQuota.total
+$used               = $org.directorySizeQuota.used
+$remaining          = $total - $used
+# Retrieving Current object counts
+$users              = (get-mguser -all).count
+$groups             = (get-mggroup -all).count
+$devices            = (get-mgdevice -all).count
+$applications       = (get-mgapplication -all).count
+$serviceprincipals  = (get-mgserviceprincipal -all).count
+$objects            = $users + $groups + $devices +$applications + $serviceprincipals
+# Retrieving Deleted Object Counts - These do count against the quota
+$deletedUserCount               = (Get-MgDirectoryDeleteduser -all).count
+$deletedgroupCount              = (Get-MgDirectoryDeletedgroup -all).count
+$deletedapplicationCount        = (Get-MgDirectoryDeletedApplication -all).count
+$deletedserviceprincipalCount   = (Get-MgDirectoryDeletedserviceprincipal -all).count
+$deleteddeviceCount             = (Get-MgDirectoryDeleteddevice -all).count
+$deletedRecoveryEstimate        = $deleteddeviceCount + $deletedserviceprincipalCount  + $deletedapplicationCount  + $deletedgroupCount + $deleteduserCount 
+# Building the Object to be exported
+$DirectoryObjectCount           =@()
+$DirectoryObjectCount           += New-Object Object |
+                                    Add-Member -NotePropertyName DirectoryQuotaTotal -NotePropertyValue $total -PassThru | 
+                                    Add-Member -NotePropertyName DirectoryQuotaUsed -NotePropertyValue $used -PassThru |
+                                    Add-Member -NotePropertyName DirectoryQuotaApproximateRemaining -NotePropertyValue $Remaining -PassThru |
+                                    Add-Member -NotePropertyName UserObjects -NotePropertyValue $users -PassThru |
+                                    Add-Member -NotePropertyName GroupObjects -NotePropertyValue $groups -PassThru |
+                                    Add-Member -NotePropertyName ApplicationObjects -NotePropertyValue $applications -PassThru |
+                                    Add-Member -NotePropertyName ServicePrincipalObjects -NotePropertyValue $serviceprincipals -PassThru |
+                                    Add-Member -NotePropertyName DeviceObjects -NotePropertyValue $devices -PassThru |
+                                    Add-Member -NotePropertyName DeletedUserObjects -NotePropertyValue $deletedUserCount -PassThru |
+                                    Add-Member -NotePropertyName DeletedGroupObjects -NotePropertyValue $deletedgroupCount -PassThru |
+                                    Add-Member -NotePropertyName DeletedApplicationObjects -NotePropertyValue $deletedapplicationCount -PassThru |
+                                    Add-Member -NotePropertyName DeletedServicePrincipalObjects -NotePropertyValue $deletedserviceprincipalCount -PassThru |
+                                    Add-Member -NotePropertyName DeletedDeviceObjects -NotePropertyValue $deleteddeviceCount -PassThru |
+                                    Add-Member -NotePropertyName DeletedRecoveryEstimate -NotePropertyValue $deletedRecoveryEstimate -PassThru 
 
-$org = get-mgorganization | select -ExpandProperty AdditionalProperties
-$total = $org.directorySizeQuota.total
-$used = $org.directorySizeQuota.used
-$remaining = $total - $used
-
-# Additionally to view the current object counts
-$users =(get-mguser -all).count
-$groups = (get-mggroup -all).count
-$devices = (get-mgdevice -all).count
-$applications = (get-mgapplication -all).count
-$serviceprincipals = (get-mgserviceprincipal -all).count
-
-$objects = $users + $groups + $devices +$applications + $serviceprincipals
-#write-host "Total counted objects : " $objects
-#write-host "Directory Quota Used : " $used
-#write-host "Directory Quota Total : " $total
-#write-host "Directory Quota Remaining : " $remaining
-
-$deletedUserCount = (Get-MgDirectoryDeleteduser -all).count
-$deletedgroupCount = (Get-MgDirectoryDeletedgroup -all).count
-$deletedapplicationCount = (Get-MgDirectoryDeletedApplication -all).count
-$deletedserviceprincipalCount = (Get-MgDirectoryDeletedserviceprincipal -all).count
-$deleteddeviceCount = (Get-MgDirectoryDeleteddevice -all).count
-$deletedRecoveryEstimate = $deleteddeviceCount + $deletedserviceprincipalCount  + $deletedapplicationCount  + $deletedgroupCount + $deleteduserCount 
-$DirectoryObjectCount =@()
-$DirectoryObjectCount += New-Object Object |
-                         Add-Member -NotePropertyName DirectoryQuotaTotal -NotePropertyValue $total -PassThru | 
-                         Add-Member -NotePropertyName DirectoryQuotaUsed -NotePropertyValue $used -PassThru |
-                         Add-Member -NotePropertyName DirectoryQuotaApproximateRemaining -NotePropertyValue $Remaining -PassThru |
-                         Add-Member -NotePropertyName UserObjects -NotePropertyValue $users -PassThru |
-                         Add-Member -NotePropertyName GroupObjects -NotePropertyValue $groups -PassThru |
-                         Add-Member -NotePropertyName ApplicationObjects -NotePropertyValue $applications -PassThru |
-                         Add-Member -NotePropertyName ServicePrincipalObjects -NotePropertyValue $serviceprincipals -PassThru |
-                         Add-Member -NotePropertyName DeviceObjects -NotePropertyValue $devices -PassThru |
-                         Add-Member -NotePropertyName DeletedUserObjects -NotePropertyValue $deletedUserCount -PassThru |
-                         Add-Member -NotePropertyName DeletedGroupObjects -NotePropertyValue $deletedgroupCount -PassThru |
-                         Add-Member -NotePropertyName DeletedApplicationObjects -NotePropertyValue $deletedapplicationCount -PassThru |
-                         Add-Member -NotePropertyName DeletedServicePrincipalObjects -NotePropertyValue $deletedserviceprincipalCount -PassThru |
-                         Add-Member -NotePropertyName DeletedDeviceObjects -NotePropertyValue $deleteddeviceCount -PassThru |
-                         Add-Member -NotePropertyName DeletedRecoveryEstimate -NotePropertyValue $deletedRecoveryEstimate -PassThru 
-
-$tdy = get-date -Format "MM-dd-yyyy hh.mm.ss"
-$file = $outputdirectory +"EntraDirectoryQuota_and_objectcounter_"+$tdy+".csv"
+$tdy                            = get-date -Format "MM-dd-yyyy hh.mm.ss"
+$file                           = $outputdirectory +"EntraDirectoryQuota_and_objectcounter_"+$tdy+".csv"
 $DirectoryObjectCount | export-csv -path $file -NoTypeInformation -Encoding UTF8
+#Rem out the display of the variable if you do not want to have this exported to the screen
 $DirectoryObjectCount | FT
 $DirectoryObjectCount | FL
